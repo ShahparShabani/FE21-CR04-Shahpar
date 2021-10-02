55efa20e008cdf3669278cae05ace10b7eade8b8
shahparshabani <shahpar.shabani@g-experts.net>
2021-10-02 20:00:33 +0200

    fix sort issue to display current like after incrementing

--- js/main.js
+++ js/main.js
@@ -1,25 +1,50 @@
+// Check if load flag dos not set, set it to true
+if (JSON.parse(localStorage.getItem(`firstLoadFlag`)) === null) {
+  localStorage.setItem(`firstLoadFlag`, true);
+}
+
+
 // Read movies from json file
 let moviesParse = JSON.parse(movies);
 
-// Set/Get movies to/from localstorage
-for (let i = 0; i < moviesParse.length; i++) {
-  localStorage.setItem(`${i}`, JSON.stringify(moviesParse[i]));
+var movieDataArray = new Array;
 
-  let movieParsedLocal = JSON.parse( localStorage.getItem(`${i}`));
-  console.log(typeof movieParsedLocal)
-  
-  let likeParsed = movieParsedLocal.like;
+createCards();
 
-  document.getElementById(
-    "movie-result"
-  ).innerHTML += `
+function createCards() {
+
+  const fisrtLoadFlag = JSON.parse(localStorage.getItem(`firstLoadFlag`))
+
+  // Check to read from json or localstorage 
+  if (fisrtLoadFlag) {
+    movieDataArray = moviesParse;
+  } else {
+    for (let i = 0; i < moviesParse.length; i++) {
+
+      // Set movies to localstorage after sort
+      movieDataArray[i] = JSON.parse(localStorage.getItem(`${i}`));
+    }
+  }
+
+  for (let i = 0; i < movieDataArray.length; i++) {
+
+    if (fisrtLoadFlag) {
+      // Set movies to localstorage before sort and on first page load
+      localStorage.setItem(`${i}`, JSON.stringify(movieDataArray[i]));
+    }
+
+    let likeParsed = movieDataArray[i].like;
+
+    document.getElementById(
+      "movie-result"
+    ).innerHTML += `
   <div class="my-css">
   <div class="card h-100 border mb-3 " style="max-width: 540px">
     <div class="row g-0">
       <div class="col-md-5">
         <img
-        src="../img/${movieParsedLocal.img
-    }"
+        src="../img/${movieDataArray[i].img
+      }"
           class="small-img img-fluid rounded-start"
           alt="Movie photo"
         />
@@ -27,15 +52,15 @@ for (let i = 0; i < moviesParse.length; i++) {
       <div class="col-md-7">
         <div class="card-body h-100 ${likeParsed ? "read" : "not-read"}">
           <h5 class="card-title"><a class="" data-bs-toggle="modal" data-bs-target="#movieModal${i}">
-          ${movieParsedLocal.name}
+          ${movieDataArray[i].name}
         </a></h5>
           <p class="card-text">
-          ${movieParsedLocal.storyline}
+          ${movieDataArray[i].storyline}
           </p>
           <p id=like-wrapper class="card-text">
             <small class="text-muted"
               ><a class="like-btn" id="a-like${i}"" data-bs-toggle="modal" data-bs-target="#movieModal2${i}">
-             <i class="fas fa-thumbs-up"></i> </a><span id="like${i}" class=dot>${movieParsedLocal.like}</span></small
+             <i class="fas fa-thumbs-up"></i> </a><span id="like${i}" class=dot>${movieDataArray[i].like}</span></small
             >
           </p>
         </div>
@@ -49,16 +74,16 @@ for (let i = 0; i < moviesParse.length; i++) {
     <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered ">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">${movieParsedLocal.name}</h5>
+          <h5 class="modal-title" id="exampleModalLabel">${movieDataArray[i].name}</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-        <img class=big-img src="../img/${movieParsedLocal.img
-    }" class="card-img-top" alt="big car photo">
-        <p>Description: ${movieParsedLocal.storyline}</p>
-        <p>Pages: ${movieParsedLocal["release-date"]}</p>
-        <p>Read: ${movieParsedLocal.like
-    } </p> 
+        <img class=big-img src="../img/${movieDataArray[i].img
+      }" class="card-img-top" alt="big car photo">
+        <p>Description: ${movieDataArray[i].storyline}</p>
+        <p>Pages: ${movieDataArray[i]["release-date"]}</p>
+        <p>Read: ${movieDataArray[i].like
+      } </p> 
           <br/>
         </div>
         <div class="modal-footer">
@@ -70,50 +95,70 @@ for (let i = 0; i < moviesParse.length; i++) {
   </div>
   `;
 
+  }
+
+  // Change load Flag
+  localStorage.setItem(`firstLoadFlag`, false);
 }
 
 
-// Get like-btn & add event listener to each one
+// Get like-btn & add event listener to each one (begin)
 const likeButtons = document.getElementsByClassName("like-btn");
-console.log(likeButtons.length);
 
 for (let i = 0; i < likeButtons.length; i++) {
 
   likeButtons[i].addEventListener('click', function () {
-   
+
     // Get related movie from Localstorage
-    let movieParsedLocal = JSON.parse( localStorage.getItem(`${i}`));
-  
-  
+    let movies = new Array();
+    movies[i] = JSON.parse(localStorage.getItem(`${i}`));
+
+
     // Increase like & update Localstorage
-    movieParsedLocal.like ++;
-    localStorage.setItem(`${i}`, JSON.stringify(movieParsedLocal));
+    movies[i].like++;
+    localStorage.setItem(`${i}`, JSON.stringify(movies[i]));
 
     // Dispaly new likes to the card
-    document.getElementById(`like${i}`).innerHTML = movieParsedLocal.like
- 
+    document.getElementById(`like${i}`).innerHTML = movies[i].like
+
   })
- 
+
 
 }
+//Get like-btn (end)
 
 
 // Sort function on descending order 
 function dynamicSort(property) {
   var sortOrder = 1;
-  if(property[0] === "-") {
-      sortOrder = -1;
-      property = property.substr(1);
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
   }
-  return function (a,b) {
-    
-      var result = (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
-      return result * sortOrder;
+  return function (a, b) {
+
+    var result = (a[property] > b[property]) ? -1 : (a[property] < b[property]) ? 1 : 0;
+    return result * sortOrder;
   }
 }
 
-//Sort based on likes
-moviesParse.sort(dynamicSort("like"));
 
-console.log(moviesParse.sort(dynamicSort("like")));
+// Add eventlistener to sort btn
+document.getElementById('sort-btn').addEventListener('click', function () {
 
+  //Sort based on likes
+  movieDataArray.sort(dynamicSort("like"));
+
+  overwriteLocalstorage();
+
+  createCards();
+
+});
+
+function overwriteLocalstorage() {
+
+  for (let i = 0; i < movieDataArray.length; i++) {
+    localStorage.setItem(`${i}`, JSON.stringify(moviesParse[i]))
+    console.log(`overwriteLocalstorage`, localStorage.getItem(`${i}`))
+  }
+}
