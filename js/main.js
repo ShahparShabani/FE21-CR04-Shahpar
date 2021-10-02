5cf09c7a6ec9b67ef116859331f1485dc189f996
shahparshabani <shahpar.shabani@g-experts.net>
2021-10-02 18:17:11 +0200

    add sort funtion

--- js/main.js
+++ js/main.js
@@ -98,4 +98,22 @@ for (let i = 0; i < likeButtons.length; i++) {
 }
 
 
+// Sort function
+function dynamicSort(property) {
+  var sortOrder = 1;
+  if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+  }
+  return function (a,b) {
+    
+      var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+      return result * sortOrder;
+  }
+}
+
+//Sort based on likes
+moviesParse.sort(dynamicSort("like"));
+
+console.log(moviesParse.sort(dynamicSort("like")));
 
